4e84b9245bf3015aff3e5c8ff8a203f5389e1faf
DomenicoScala <dome.scala12@gmail.com>
2023-11-30 17:44:34 +0100

    Fine Esercizio

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+const Nome = prompt ('Nome')
+const Cognome = prompt ('Cognome')
+const ColorePreferito = prompt ('Colore Preferito')
+const Numero = prompt('Numero')
+
+let risultatoTotale = (Nome + ' ' + Cognome + ' ' + ColorePreferito + ' ' + Numero);
+document.writeln(risultatoTotale);
